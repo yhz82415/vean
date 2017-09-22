a7aa38c105ea6086a9fb68ec12bcab1950577a5b
Evan Yang <eurekaaj@gmail.com>
2017-09-21 22:43:50 -0700

    sum of all numbers that for formed from root to leaf paths. add description

--- src/main/java/tree/SumOfAllNumbersFromed.java
+++ src/main/java/tree/SumOfAllNumbersFromed.java
@@ -1,5 +1,25 @@
 package tree;
 
+/**
+ * Given a binary tree, where every node value is a Digit from 1-9 .Find the sum of all the numbers which are formed from root to leaf paths.
+
+ For example consider the following Binary Tree.
+
+          6
+       /      \
+     3          5
+   /   \          \
+ 2     5          4
+     /   \
+    7     4
+ There are 4 leaves, hence 4 root to leaf paths:
+ Path                    Number
+ 6->3->2                   632
+ 6->3->5->7               6357
+ 6->3->5->4               6354
+ 6->5>4                    654
+ Answer = 632 + 6357 + 6354 + 654 = 13997
+ */
 public class SumOfAllNumbersFromed {
 
     public static void main(String... args) {
